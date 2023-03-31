b63f5f7f47e93c3ba8f32ba180da7783db804e64
유빈 <bin00509@sungkyul.ac.kr>
2023-03-31 06:15:53 +0000

    1

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 <script type="text/javascript" src="js/search.js" defer></script>
+
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
-	alert("검색을 수행합니다~!");
-}
+   alert("검색을 수행합니다!");}
