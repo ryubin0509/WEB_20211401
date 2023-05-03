cf9b69232de55d4f2ac4620c4251757ba417bcae
유빈 <bin00509@sungkyul.ac.kr>
2023-05-03 05:21:19 +0000

    dwqdwqd

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
+document.getElementById("search_btn").addEventListener('click',search_message);
 
-var search_array = []; // 빈 배열 – 전역 변수
+var search_array = []; // 빈 배열 – 전역 변수 
 
 function search_message(){
    let search_str = document.querySelector("#search_txt");
@@ -9,9 +9,14 @@ function search_message(){
     }
     else{
        alert("검색을 수행합니다!");
-       search_array.push(search_str.value); // 배열에 검색어 추가
-       let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-        document.querySelector("#form_main").submit();
+       search_array.push(search_str.value);
+       let text = document.getElementById("search_message").innerHTML = search_array.toString();
+
+       // form 요소가 있는지 확인하고 호출
+       let form = document.querySelector("#form_main");
+       if (form) {
+         form.submit();
+       }
     }
 }
 
@@ -22,11 +27,40 @@ function search_message(){
 
 
 
+/*function search_message(){    alert("검색을 수행합니다!"); 
+function search_message(){ let search_str = document.querySelector("#search_txt"); // 변수에 저장
+ if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+	 alert("검색어가 비었습니다. 입력해주세요"); } 
+else{
+	alert("검색을 수행합니다!");  
+    search_array.push(search_str.value); // 배열에 검색어 추가  
+// let text = document.getElementById("search_message").innerHTML = search_str.value;
+let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환  
+	document.querySelector("#form_main");   
+if(form){
+	form.submit(); 
+}
+}
+						 }
+
+//cument.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가   //onsole.log
 
 
+/*documentment.getElementById("search_btn").addEventListener('click',search_message);
 
+var search_array = []; // 빈 배열 – 전역 변수
 
-
+function search_message(){
+   let search_str = document.querySelector("#search_txt");
+    if(search_str.value.length === 0){
+       alert("검색어가 비었습니다. 입력해주세요"); 
+    }
+    else{
+      alert("검색을 수행합니다!");
+       search_array.push(search_str.value); // 배열에 검색어 추가
+       let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+        document.querySelector("#form_main").submit();
+    }
 
 
 
@@ -58,4 +92,4 @@ if(search_str.value.length === 0){
    console.log(search_str.value); // 콘솔에 출력 */
 
 
-
+						 
