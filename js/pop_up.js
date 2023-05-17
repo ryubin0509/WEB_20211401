f602118de7caaade5b5589bb0cad5c9be8110420
유빈 <bin00509@sungkyul.ac.kr>
2023-05-17 07:02:36 +0000

    1

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,7 +1,9 @@
 function pop_up(){
-	
-	window.open("../pop_up/pop_up.html"," 팝업테스트" ,"width= 400, height= 300, top=10 , left=10 ");
-}
+	var cookieCheck = getCookie("popupYN");
+	if (cookieCheck != "N"){
+	window.open("../pop_up.html"," 팝업테스트" ,"width= 400, height= 300, top=10 , left=10 ");
+  }
+}	
 function showclock(){ 
 	let currentDate = new Date(); // 날짜 객체 생성
 	let divClock = document.getElementById('divClock');
@@ -26,4 +28,34 @@ function showclock(){
 	setTimeout(show_clock, 1000); // 1초마다 갱신
 }
 
+     function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite= None; Secure";       
+    }
 
+      function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "popupYN") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return ;
+}
+
+function closePopup() {
+        if (document.getElementById('check_popup').value) {
+            setCookie("popupYN", "N", 1);
+            console.log("쿠키를 설정합니다.");
+            self.close();
+        }
+    
+
+}
