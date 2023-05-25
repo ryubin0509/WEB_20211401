c1293c8db13bf83406ec9eccfc2682adf33c2a82
유빈 <bin00509@sungkyul.ac.kr>
2023-05-25 15:12:45 +0000

    1

---
+++ js/cookie.js
@@ -0,0 +1,17 @@
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+    
+    if(get_id) { 
+    id.value = get_id; 
+    check.checked = true; 
+    }
+	session_check(); //세션 유무 검사
+}
+
+function deleteCookie(cookieName){
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
--- js/login.js
+++ js/login.js
@@ -1,24 +1,15 @@
-function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
-    let id = document.querySelector("#floatingInput");
-    let check = document.querySelector("#idSaveCheck");
-    let get_id = getCookie("id");
-    
-    if(get_id) { 
-    id.value = get_id; 
-    check.checked = true; 
-    }
-	session_check(); //세션 유무 검사
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
 }
 
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
 
-
-function session_check() { //세션 검사 
-if(sessionStorage.getItem("Session_Storage_test")){
-	alert("이미 로그인 되었습니다."); 
-	location.href='index_login.html'; //로그인된 페이지로 이동 
-}
-
-}
 
 
 function login(){
@@ -50,37 +41,11 @@ function login(){
     }
 }
 
-function session_del() {//세션 삭제
-    // Check if the sessionStorage object exists
-    if (sessionStorage) {
-        // Retrieve data
-        sessionStorage.removeItem("Session_Storage_test");
-        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
-    } else {
-        alert("세션 스토리지 지원 x");
-    }
-}
-
-
-
-
-
-
-
-
-
 function logout(){
 	session_del(); //세션 삭제
     location.href='../index.html';
 }
-function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
-}
 
-
- 
 
 
 function get_id(){
@@ -111,58 +76,7 @@ alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
         date.setDate(date.getDate() + expiredays);
         document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite= None; Secure";       
     }
-function session_set() { //세션 저장
-    let id = document.querySelector("#floatingInput");
-	let password = document.querySelector("#floatingPassword");
-	if(sessionStorage){
-		let en_text = encrypt_text(password.value);
-		sessionStorage.setItem("Session_Storage_test",en_text)
-	
 
-    } else {
-        alert("로컬 스토리지 지원 x");
-    }
-}
-function session_get() { //세션 읽기
-    if (sessionStorage) {
-       return sessionStorage.getItem("Session_Storage_test");
-    } else {
-        alert("세션 스토리지 지원 x");
-    }
-}
-
-function encodeByAES256(key, data){
-    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
-        iv: CryptoJS.enc.Utf8.parse(""),
-        padding: CryptoJS.pad.Pkcs7,
-        mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString();
-}
-function decodeByAES256(key, data){
-    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
-        iv: CryptoJS.enc.Utf8.parse(""),
-        padding: CryptoJS.pad.Pkcs7,
-        mode: CryptoJS.mode.CBC
-    });
-    return cipher.toString(CryptoJS.enc.Utf8);
-};
-function encrypt_text(password){
-    const k = "key"; // 클라이언트 키
-    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const b = password;
-    const eb = this.encodeByAES256(rk, b);
-    return eb;
-    console.log(eb);
-}
-
-function decrypt_text(){
-    const k = "key"; // 서버의 키
-    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-    const eb = session_get();
-    const b = this.decodeByAES256(rk, eb);
-    console.log(b); 
-}
 
       function getCookie(name) {
         var cookie = document.cookie;
