087c2cdf3a01c1a4c6e97e2f3db93620b55aa224
유빈 <bin00509@sungkyul.ac.kr>
2023-06-09 09:03:06 +0000

    1

--- js/search.js
+++ js/search.js
@@ -1,26 +1,42 @@
-document.getElementById("search_btn").addEventListener('click',search_message);
+document.getElementById("search_btn").addEventListener('click', search_message);
 
-var search_array = []; // 빈 배열 – 전역 변수 
+// 검색어로 적절하지 않은 문자열을 담은 배열 – 전역 변수
+const badWords = ["시발", "씨발", "병신"];
 
-function search_message(){
-   let search_str = document.querySelector("#search_txt");
-    if(search_str.value.length === 0){
-       alert("검색어가 비었습니다. 입력해주세요"); 
+// 검색어를 검증하는 함수
+function no_str(str) {
+  for (const word of badWords) {
+    if (str.includes(word)) {
+      return false;
     }
-    else{
-       alert("검색을 수행합니다!");
-       search_array.push(search_str.value);
-       let text = document.getElementById("search_message").innerHTML = search_array.toString();
-
-       // form 요소가 있는지 확인하고 호출
-       let form = document.querySelector("#form_main");
-       if (form) {
-         form.submit();
-       }
-    }
+  }
+  return true;
 }
 
+// 검색어를 저장하는 배열 – 전역 변수
+const search_array = [];
 
+function search_message() {
+  const search_str = document.querySelector("#search_txt");
+  if (search_str.value.length === 0) {
+    alert("검색어가 비었습니다. 입력해주세요");
+    return;
+  }
+
+  if (!no_str(search_str.value)) {  // 검색어가 적절하지 않은 경우
+    alert(`${search_str.value}은(는) 검색어로 적절하지 않습니다.`);
+    return  }
+
+  alert("검색을 수행합니다!");
+  search_array.push(search_str.value);
+  let text = document.getElementById("search_message").innerHTML = search_array.toString();
+
+  // form 요소가 있는지 확인하고 호출
+  let form = document.querySelector("#form_main");
+  if (form) {
+    form.submit();
+  }
+}
 
 
 
